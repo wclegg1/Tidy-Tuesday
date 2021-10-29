5350f34d7ec0f0915e3ce8eb2dcd8938d37afbc3
WC <wyatclegg@gmail.com>
2021-10-29 11:01:25 -0600

    EDA

--- tt_10_26.R
+++ tt_10_26.R
@@ -6,7 +6,15 @@
 # Define Libraries & Functions --------------------------------------------
 library(tidyverse)
 
-
 # Read In Data ------------------------------------------------------------
+ultra_rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/ultra_rankings.csv')
+race <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/race.csv')
 
+
+# EDA ---------------------------------------------------------------------
+ggplot(race %>% filter(participation != "solo", distance > 100),
+       aes(x = date, y = distance, col = elevation_gain)) + 
+  geom_point() 
+race %>% filter(participation != "solo", distance < 10) %>% 
+  as.data.frame
 
