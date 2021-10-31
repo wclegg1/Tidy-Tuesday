e0207e139fb5bba69f5fbb3a37f528d746f6af71
WC <wyatclegg@gmail.com>
2021-10-31 11:37:45 -0600

    Sunday Morning Push

--- tt_10_26.R
+++ tt_10_26.R
@@ -5,9 +5,13 @@
 
 # Define Libraries & Functions --------------------------------------------
 library(tidyverse)
+library(tidymodels)
 
 # Read In Data ------------------------------------------------------------
-ultra_rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/ultra_rankings.csv')
+ultra_rankings <- 
+  readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/ultra_rankings.csv') %>% 
+  mutate(nationality_Abb = if_else(nationality %in% c("USA", "FRA", "GBR", "JPN", "ESP", "CHN", "CAN"), nationality,
+                                   "Other"))
 race <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-26/race.csv')
 
 
@@ -18,3 +22,15 @@ ggplot(race %>% filter(participation != "solo", distance > 100),
 race %>% filter(participation != "solo", distance < 10) %>% 
   as.data.frame
 
+ggplot(ultra_rankings %>% drop_na(gender),
+       aes(x = time_in_seconds, y = rank, col = nationality_Abb)) + 
+  geom_point(size = 0.3) + facet_wrap(~gender + nationality_Abb) + theme_bw() +
+  scale_color_brewer(palette = "Set1")
+
+finalData <- ultra_rankings %>% drop_na(gender) %>% 
+  left_join(race, by = c("race_year_id"))
+
+
+# Fit Models --------------------------------------------------------------
+
+fit1 <- lm(time_in_seconds ~ age + gender + elevation_gain, data = finalData)
